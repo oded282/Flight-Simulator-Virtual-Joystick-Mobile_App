b0e2052e11d828bb8940efad9debdaf9503b6fc5
oded282 <oded282@gmail.com>
2019-06-16 21:16:12 +0300

    2115

--- app/src/main/java/com/example/ex4/MainActivity.java
+++ app/src/main/java/com/example/ex4/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.ex4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void ConnectButton(View view){
+
+        EditText ipText = (EditText) findViewById(R.id.ip);
+        EditText portText = (EditText) findViewById(R.id.port);
+        String ip = ipText.getText().toString();
+        String port = portText.getText().toString();
+
+        Intent intent = new Intent(this, JoystickActivity.class);
+
+        intent.putExtra("ip" , ip);
+        intent.putExtra("port" , port);
+
+        startActivity(intent);
+    }
+
+
 }
